957b720fb131077a6087e6e4b34f8a537cafe572
Vinicius Nogueira (Mac) <vinicius.nogueira@unidesk.com.br>
2021-03-28 15:39:47 -0300

    Módulo 25: Room - Biblioteca para bancod de dados aula 144

--- app/src/main/java/com/example/convidados/model/GuestModel.java
+++ app/src/main/java/com/example/convidados/model/GuestModel.java
@@ -1,9 +1,20 @@
 package com.example.convidados.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "guest")
 public class GuestModel {
 
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true)
     private int id;
+
+    @ColumnInfo(name = "name")
     private String name;
+
+    @ColumnInfo(name = "confirmation")
     private int confirmation;
 
     public GuestModel(int id, String name, int confirmation) {
---
+++ app/src/main/java/com/example/convidados/repository/GuestDataBase.java
@@ -0,0 +1,41 @@
+package com.example.convidados.repository;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.convidados.model.GuestModel;
+
+@Database(entities = {GuestModel.class}, version = 1)
+public abstract class GuestDataBase extends RoomDatabase {
+
+    public static GuestDataBase INSTANTECE;
+    public static GuestDataBase getDataBase(Context context) {
+
+        if (INSTANTECE != null)
+            INSTANTECE = Room.databaseBuilder(context, GuestDataBase.class, "convidados")
+                    .allowMainThreadQueries()
+                    .addCallback(new Callback() {
+                        @Override
+                        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                            super.onCreate(db);
+                        }
+                    })
+                    .addMigrations(MIGRATION_1_2)
+                    .build();
+
+        return INSTANTECE;
+    }
+
+    private static Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+
+        }
+    };
+}
